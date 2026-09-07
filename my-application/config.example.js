36b5293f8e251f979de1eaa0ef8c333b423bd204
Shogo0514 <chochokun07@gmail.com>
2026-09-08 01:13:16 +0900

    Delete my-application directory

--- my-application/config.example.js
+++
@@ -1,4 +0,0 @@
-window.__MY_APP_CONFIG__ = {
-  SUPABASE_URL: "https://your-project.supabase.co",
-  SUPABASE_ANON_KEY: "your-anon-or-publishable-key",
-};
--- my-application/config.js
+++
@@ -1,6 +0,0 @@
-// Supabaseの公開URLと公開キーを設定してください。
-// ここに入れるのは anon / publishable key です。service_role key は絶対に入れないでください。
-window.__MY_APP_CONFIG__ = {
-  SUPABASE_URL: "https://cxecdwseyesundvixfqq.supabase.co/rest/v1/",
-  SUPABASE_ANON_KEY: "sb_publishable_AR_NENscra2DuONe_MKsCQ_i07AKexc",
-};
