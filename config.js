f883d96f10be361e64c017257e93c10418dd7f9e
Shogo0514 <chochokun07@gmail.com>
2026-09-08 01:18:19 +0900

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabaseの公開URLと公開キーを設定してください。
 // ここに入れるのは anon / publishable key です。service_role key は絶対に入れないでください。
 window.__MY_APP_CONFIG__ = {
-  SUPABASE_URL: "https://cxecdwseyesundvixfqq.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://cxecdwseyesundvixfqq.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_AR_NENscra2DuONe_MKsCQ_i07AKexc",
 };
